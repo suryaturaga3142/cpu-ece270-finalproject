e555d87d2ba1cba13f10d1256f3dd93483da50eb
ap-bruh-2005 <adityapprabhu@gmail.com>
2025-04-20 16:01:42 -0400

    memory_controller

--- memory_controller.sv
+++ memory_controller.sv
@@ -1,6 +1,5 @@
 `include "params.svh"
 
-
 module memory_controller(
     input  logic  clk,
     input  logic  rst,
@@ -14,10 +13,9 @@ module memory_controller(
     output logic [BUS_WIDTH-1:0]  din,
     output logic [BUS_WIDTH-1:0]  read_data,
     output logic  write_en,
-    output logic  busy,
+    output logic  busy
 );
 
-typedef enum [2:0] {IDLE, WRITE, READ} memory_state;
 memory_state curr_state, next_state; 
 
 always_ff @(posedge clk or posedge rst) begin
@@ -36,7 +34,7 @@ always_comb begin
             end else if(write_read[1] == 1'b0 && write_read[0] == 1'b1) begin
                 next_state = READ; 
             end else begin
-                next_state = IDLE:  
+                next_state = IDLE;  
             end 
         end
 
@@ -44,16 +42,16 @@ always_comb begin
 
         READ : next_state = IDLE; 
     
-        default : next_state = IDLE: 
+        default : next_state = IDLE; 
     endcase
 end
 
-assign write_en = (state == WRITE) ? 1'b1 : 1'b0; 
+assign write_en = (curr_state == WRITE) ? 1'b1 : 1'b0; 
 assign waddr = addr; 
 assign raddr = addr;
 assign din = write_data; 
-assign read_data = (state == READ) ? ram_dout : 2'h00; 
-assign busy = (state != IDLE); 
+assign read_data = (curr_state == READ) ? ram_dout : '0; 
+assign busy = (curr_state != IDLE); 
 
 
 
